f95d31a661d0acaa8cb1fd030c949c1dcdda9488
Sergio Damaceno Botelho <sergio.damaceno0002@gmail.com>
2021-10-20 01:52:35 -0300

    Primeiro Commit Aula 1

---
+++ src/services/CreateMessageService.ts
@@ -0,0 +1,18 @@
+import prismaClient from '../prisma';
+
+
+class CreateMessageService{
+    async execute(text: string, user_id: string){
+        const message = await prismaClient.message.create({
+            data: {
+                text,
+                user_id
+            },
+            included: {
+                user: true,
+            },
+        });
+        return message;
+    }
+}
+export { CreateMessageService }; 
